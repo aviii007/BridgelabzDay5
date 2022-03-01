2d1362b8841390a20c06c28b705f4332343dcba3
Ashvini Kanojia <99378217+aviii007@users.noreply.github.com>
2022-03-01 11:15:32 +0530

    Add files via upload

---
+++ src/com/java/day5/CoinFlip.java
@@ -0,0 +1,11 @@
+package com.java.day5;
+
+public class CoinFlip {
+    public static void main(String[] args) {
+        if (Math.random() < 0.5) {
+            System.out.println("Tails");
+        } else {
+            System.out.println("Heads");
+        }
+    }
+}
